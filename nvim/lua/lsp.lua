13063859936c2a3bdcc2447ba1b66f248eee99ef
albarrync <albarrync@gmail.com>
2024-06-25 15:39:04 -0400

    More LSP Cleanup

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -12,62 +12,62 @@ end
 vim.cmd('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
 
 nvim_lsp.dockerls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.jsonls.setup{
   commands = { Format = { function() vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0}) end } },
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.yamlls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.vimls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.dartls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.tsserver.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.cssls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.pyright.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.clojure_lsp.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.elixirls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.terraformls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.pylsp.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.lua_ls.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 -- If they're not working, try running `gem install solargraph` and `gem install ruby-lsp`
 -- ruby-lsp requires ruby version 2.7.3 or higher and nvim 0.10.0
 nvim_lsp.ruby_lsp.setup{
   cmd = { "/Users/alexb/.rbenv/shims/ruby-lsp"},
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.solargraph.setup{
   cmd = { "/Users/alexb/.rbenv/shims/solargraph", "stdio"},
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.standardrb.setup{
   cmd = { "/Users/alexb/.rbenv/shims/standardrb", "--lsp"},
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.tailwindcss.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
 nvim_lsp.prismals.setup{
-  capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  capabilities = cmp.default_capabilities(capabilities)
 }
